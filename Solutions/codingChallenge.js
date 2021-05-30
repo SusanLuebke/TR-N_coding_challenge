c7968522ae8ea02c97afbf44304b3a9ba0eb6b38
Susan Luebke <78167908+SusanLuebke@users.noreply.github.com>
2021-05-30 18:16:46 +0100

    Refactored with beforeEach and used Ternary ?, :

--- Solutions/codingChallenge.js
+++ Solutions/codingChallenge.js
@@ -1,32 +1,33 @@
+// Reference Dictionary
 const compassPoints = {
-N: {
-    L: 'W',
-    R: 'E',
-    move: (x, y)=>{
-    return {x:x, y:y+1};
+  N: {
+    L: "W",
+    R: "E",
+    move: (x, y) => {
+      return { x: x, y: y + 1 };
     },
-},
-E: {
-    L: 'N',
-    R: 'S',
-    move: (x, y)=>{
-    return {x:x+1, y:y};
+  },
+  E: {
+    L: "N",
+    R: "S",
+    move: (x, y) => {
+      return { x: x + 1, y: y };
     },
-},
-S: {
-    L: 'E',
-    R: 'W',
-    move: (x, y)=>{
-    return {x:x, y:y-1};
+  },
+  S: {
+    L: "E",
+    R: "W",
+    move: (x, y) => {
+      return { x: x, y: y - 1 };
     },
-},
-W: {
-    L: 'S',
-    R: 'N',
-    move: (x, y)=>{
-    return {x:x-1, y:y};
+  },
+  W: {
+    L: "S",
+    R: "N",
+    move: (x, y) => {
+      return { x: x - 1, y: y };
     },
-},
+  },
 };
 
 // move, direction, possibly track (class structure first, restructure with functional prog)
@@ -35,29 +36,23 @@ W: {
 // return the command when passed through
 
 class marsRover {
-    constructor(position, direction) {
-        this.currentPosition_xy = position;
-        this.compassPoints = compassPoints;
-        this.direction = direction;
-    }
+  constructor(currentPosition, direction, obstacles) {
+    this.currentPosition_xy =
+      (this.currentPosition_xy === undefined) ? [0, 0] : this.currentPosition_xy;
+    this.direction = (this.direction === undefined) ? "N" : this.direction; // NEWS
+    this.obstacles = (this.obstacles === undefined) ? [] : this.obstacles;
+    this.compassPoints = compassPoints;
 
-    rotate(direction) {
-        return "E";
+  }
 
-    }
-} 
+  rotate(direction) {
+    return "E";
+  }
 
-
-
-
-
-
-
-
+  // move()
+}
 
 // in order to export
 module.exports = {
-    marsRover,
+  marsRover,
 };
-
-
--- Tests/codingChallenge.test.js
+++ Tests/codingChallenge.test.js
@@ -1,24 +1,27 @@
 const { marsRover } = require("../Solutions/codingChallenge");
 
-// beforeEach(() => {
-//   rover = new Rover();
-// });
+beforeEach(() => {
+  newRover = new marsRover([0,0], "N");
+});
 
-describe("marsRover", () => {
-  test("Has an initial position of [0, 0]", () => {
-    let newRover = new marsRover([0, 0], "N");
+describe("marsRover", () => {  
+  test("Has an initial position of [0, 0], 'N'", () => {
     expect(newRover.currentPosition_xy).toEqual([0, 0]);
     expect(newRover.direction).toEqual("N");
   });
 
   test("With compassPoint N, 'L' point is 'W'", () => {
-    let newRover = new marsRover();
     expect(newRover.compassPoints[`N`][`L`]).toEqual("W");
   });
 
   test("Rotate right signifies east, 'E'", () => {
-    let newRover = new marsRover([0,0], 'N');
     expect(newRover.rotate(`R`)).toEqual("E");
   });
+
+  test("Rover can move when given instructions", () => {
+    newRover.move('LMLM')
+    expect(newRover.currentPosition_xy).toEqual([1, 1]);
+  });
+
 
 });
