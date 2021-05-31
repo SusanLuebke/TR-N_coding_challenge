4718de86549e91905c976628b5c33ba206cf96eb
Susan Luebke <78167908+SusanLuebke@users.noreply.github.com>
2021-05-31 14:46:17 +0100

    TDD Edge/roll case tests pass (top, bottom, L and R)

--- Solutions/codingChallenge.js
+++ Solutions/codingChallenge.js
@@ -1,63 +1,87 @@
 // Reference Dictionary
-const compassPoints = {
-  N: {
-    L: "W",
-    R: "E",
-    move: (x, y) => {
-      return { x: x, y: y + 1 };
-    },
-  },
-  E: {
-    L: "N",
-    R: "S",
-    move: (x, y) => {
-      return { x: x + 1, y: y };
-    },
-  },
-  S: {
-    L: "E",
-    R: "W",
-    move: (x, y) => {
-      return { x: x, y: y - 1 };
-    },
-  },
-  W: {
-    L: "S",
-    R: "N",
-    move: (x, y) => {
-      return { x: x - 1, y: y };
-    },
-  },
-};
-
-// move, direction, possibly track (class structure first, restructure with functional prog)
-// classes rover, plateau
-// rotate, move, execute
-// return the command when passed through
+// const compassPoints = {
+//   N: {
+//     L: "W",
+//     R: "E",
+//     move: (x, y) => {
+//       return { x: x, y: y + 1 };
+//     },
+//   },
+//   E: {
+//     L: "N",
+//     R: "S",
+//     move: (x, y) => {
+//       return { x: x + 1, y: y };
+//     },
+//   },
+//   S: {
+//     L: "E",
+//     R: "W",
+//     move: (x, y) => {
+//       return { x: x, y: y - 1 };
+//     },
+//   },
+//   W: {
+//     L: "S",
+//     R: "N",
+//     move: (x, y) => {
+//       return { x: x - 1, y: y };
+//     },
+//   },
+// };
 
 class marsRover {
-  constructor(currentPosition, direction, obstacles) {
+  constructor(currentPosition_xy, direction, obstacles) {
     this.currentPosition_xy =
-      this.currentPosition_xy === undefined ? [0, 0] : this.currentPosition_xy;
-    this.direction = this.direction === undefined ? "N" : this.direction; // NEWS, LR
-    this.obstacles = this.obstacles === undefined ? [] : this.obstacles;
-    this.compassPoints = compassPoints;
+      this.currentPosition_xy === undefined ? [0, 0] : currentPosition_xy;
+    this.direction = this.direction === undefined ? "N" : direction; // NESW, LR
+    this.obstacles = this.obstacles === undefined ? [] : obstacles;
+    this.compassPoints = ["N", "E", "S", "W"];
+    this.compassPointsIndex = 0;
   }
 
-  rotate(direction) {
-    return "E";
+  rotate(leftOrRight) {
+    if (leftOrRight === "L") {
+      this.compassPointsIndex = this.compassPointsIndex - 1;
+      if (this.compassPointsIndex < 0) {
+        this.compassPointsIndex += 4;
+      }
+    } else {
+      this.compassPointsIndex = (this.compassPointsIndex + 1) % 4;
+    }
+    this.direction = this.compassPoints[this.compassPointsIndex];
+    return this.direction;
   }
 
   move() {
-    if ((this.direction = "N")) {
-      this.currentPosition_xy[1] += 1;
-    } else if ((this.direction = "E")) {
-      this.currentPosition_xy[0] += 1;
-    } else if ((this.direction = "W")) {
+    if (this.direction === "N") {
+      this.currentPosition_xy[1] = (this.currentPosition_xy[1] + 1) % 6;
+    } else if (this.direction === "E") {
+      this.currentPosition_xy[0] = (this.currentPosition_xy[0] + 1) % 6;
+    } else if (this.direction === "W") {
       this.currentPosition_xy[0] -= 1;
-    } else if ((this.direction = "S")) {
+      if (this.currentPosition_xy[0] < 0) { // addresses roll over case, neg
+        this.currentPosition_xy[0] += 6;
+      }
+    } else if (this.direction === "S") {
       this.currentPosition_xy[1] -= 1;
+      if (this.currentPosition_xy[1] < 0) { // addresses roll over case, neg
+        this.currentPosition_xy[1] += 6;
+      }
     }
+  }
+
+  execute(command) {
+    let commands = command.split(``);
+    for (let i = 0; i < commands.length; i++) {
+      if (commands[i] === "L" || commands[i] === "R") {
+        this.rotate(commands[i]);
+      } else {
+        this.move();
+      }
+    }
+
+    return [this.currentPosition_xy, this.direction];
   }
 }
 
--- Tests/codingChallenge.test.js
+++ Tests/codingChallenge.test.js
@@ -10,9 +10,9 @@ describe("marsRover", () => {
     expect(newRover.direction).toEqual("N");
   });
 
-  test("With compassPoint N, 'L' point is 'W'", () => {
-    expect(newRover.compassPoints[`N`][`L`]).toEqual("W");
-  });
+  // test("With compassPoint N, 'L' point is 'W'", () => {
+  //   expect(newRover.compassPoints[`N`][`L`]).toEqual("W");
+  // });
 
   test("Rotate right signifies east, 'E'", () => {
     expect(newRover.rotate(`R`)).toEqual("E");
@@ -21,6 +21,44 @@ describe("marsRover", () => {
   test("Rover can move when given instructions", () => {
     newRover.move()
     expect(newRover.currentPosition_xy).toEqual([0, 1]);
+  });
+
+  test("Rover can execute a multifaceted command", () => {
+    let command = 'RM'
+    answer = newRover.execute(command)
+    expect(answer).toEqual([[1, 0], 'E']);
+  });
+
+  // Test for rollover/ edge cases; right, left, top and bottom
+
+  test("Tests for right roll over edge case", () => {
+    let command = 'RMMMMMM' // edge case off grid right
+    answer = newRover.execute(command)
+    expect(answer).toEqual([[0, 0], 'E']);
+  });
+
+  test("Tests for top roll over edge case", () => {
+    let command = 'MMMMMM' // edge case off grid top
+    answer = newRover.execute(command)
+    expect(answer).toEqual([[0, 0], 'N']);
+  });
+
+  test("Tests for rotate roll over", () => {
+    let command = 'RRRR' // edge case off right of compassPoint
+    answer = newRover.execute(command)
+    expect(answer).toEqual([[0, 0], 'N']);
+  });
+
+  test("Tests for rotate roll over, on lefthand side", () => {
+    let command = 'L' // edge case off left of compassPoint
+    answer = newRover.execute(command)
+    expect(answer).toEqual([[0, 0], 'W']);
+  });
+
+  test("Tests for bottom roll over edge case", () => {
+    let command = 'LLM' // edge case off grid bottom
+    answer = newRover.execute(command)
+    expect(answer).toEqual([[0, 5], 'S']);
   });
 
 
