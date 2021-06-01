4718044777b2cdcbe0470305f500dfb75009798c
Susan Luebke <78167908+SusanLuebke@users.noreply.github.com>
2021-06-01 16:30:15 +0100

    Google Style Guide, prettier, code clean up

--- Solutions/codingChallenge.js
+++ Solutions/codingChallenge.js
@@ -1,19 +1,31 @@
-// Mars Rover: Perseverence
+// Mars Rover Kata: Perseverence
+
 class marsRover {
+  /**
+   * Mars Rovers - create a program to move rovers around a grid plateau. A rover’s position and location are represented by a combination of x and y coordinates and a letter representing one of the four cardinal compass points (N, E, S, W). For movement, NASA sends a string of letters, 'L', 'R' and 'M', for left, right and move respectively.
+   *
+   * Args
+   * -----
+   * @param {array} currentPosition_xy - the input, updated based on command
+   * @param {string} direction - current direction, N, E, S or W, rover is facing; used in conjuction with currentPosition_xy
+   * @param {array} grid_xy - The upper right coordinates of the plateau; lower left coordinates assumed to be 0, 0
+   * @param {array} obstacles - An array of x,y coordinates of known obstacle locations on the plateau; dead rovers, rocks, canyons, etc.
+   */
+
   constructor(currentPosition_xy, direction, grid_xy, obstacles) {
     this.grid_xy = grid_xy === undefined ? [5, 5] : grid_xy;
     this.currentPosition_xy =
       currentPosition_xy === undefined ? [0, 0] : currentPosition_xy;
-    this.direction = direction === undefined ? "N" : direction; // NESW, LR
+    this.direction = direction === undefined ? 'N' : direction; // NESW, LR
     this.obstacles = obstacles === undefined ? [] : obstacles;
-    this.compassPoints = ["N", "E", "S", "W"];
+    this.compassPoints = ['N', 'E', 'S', 'W'];
     this.compassPointsIndex = this.compassPoints.indexOf(this.direction); // to accept any initial postion beyond N
   }
 
   // To rotate Rover to the Left(-) or Right(+)
   rotate(leftOrRight) {
     // turn L or R
-    if (leftOrRight === "L") {
+    if (leftOrRight === 'L') {
       this.compassPointsIndex = this.compassPointsIndex - 1; // left is -1
       if (this.compassPointsIndex < 0) {
         this.compassPointsIndex += this.compassPoints.length;
@@ -30,17 +42,17 @@ class marsRover {
   // Includes ability to handle roll over case, negative value
   move() {
     console.log(this.grid_xy);
-    if (this.direction === "N") {
+    if (this.direction === 'N') {
       if (!this.checkObstacles()) {
         this.currentPosition_xy[1] =
-          (this.currentPosition_xy[1] + 1) % (this.grid_xy[1] + 1);
+          (this.currentPosition_xy[1] + 1) % (this.grid_xy[1] + 1); // use modulo to return 0 for pos no.
       }
-    } else if (this.direction === "E") {
+    } else if (this.direction === 'E') {
       if (!this.checkObstacles()) {
         this.currentPosition_xy[0] =
-          (this.currentPosition_xy[0] + 1) % (this.grid_xy[0] + 1);
+          (this.currentPosition_xy[0] + 1) % (this.grid_xy[0] + 1); // use modulo to return 0 for pos no.
       }
-    } else if (this.direction === "S") {
+    } else if (this.direction === 'S') {
       if (!this.checkObstacles()) {
         this.currentPosition_xy[1] -= 1;
         if (this.currentPosition_xy[1] < 0) {
@@ -48,7 +60,7 @@ class marsRover {
           this.currentPosition_xy[1] += this.grid_xy[1] + 1;
         }
       }
-    } else if (this.direction === "W") {
+    } else if (this.direction === 'W') {
       if (!this.checkObstacles()) {
         this.currentPosition_xy[0] -= 1;
         if (this.currentPosition_xy[0] < 0) {
@@ -63,7 +75,7 @@ class marsRover {
   execute(command) {
     let commands = command.split(``);
     for (let i = 0; i < commands.length; i++) {
-      if (commands[i] === "L" || commands[i] === "R") {
+      if (commands[i] === 'L' || commands[i] === 'R') {
         this.rotate(commands[i]);
       } else {
         this.move();
@@ -73,6 +85,7 @@ class marsRover {
     return [this.currentPosition_xy, this.direction];
   }
 
+  // Future bolt on to test for obstacles, currently hard coded to false
   checkObstacles(currentPosition_xy, direction) {
     // Check if an obstacle will come into play
     return false;
--- Tests/codingChallenge.test.js
+++ Tests/codingChallenge.test.js
@@ -1,77 +1,77 @@
-const { marsRover } = require("../Solutions/codingChallenge");
+const { marsRover } = require('../Solutions/codingChallenge');
 
 // Default setting is [0, 0], 'N' of a 5 x 5 grid
 beforeEach(() => {
-  newRover = new marsRover([0, 0], "N", [5, 5]);
+  newRover = new marsRover([0, 0], 'N', [5, 5]);
 });
 
-describe("marsRover", () => {
+describe('marsRover', () => {
   test("Has an initial position of [0, 0], 'N'", () => {
     expect(newRover.currentPosition_xy).toEqual([0, 0]);
-    expect(newRover.direction).toEqual("N");
+    expect(newRover.direction).toEqual('N');
   });
 
   test("A rotate right signifies east, 'E'", () => {
-    expect(newRover.rotate(`R`)).toEqual("E");
+    expect(newRover.rotate(`R`)).toEqual('E');
   });
 
-  test("Rover can move when given instructions", () => {
+  test('Rover can move when given instructions', () => {
     newRover.move();
     expect(newRover.currentPosition_xy).toEqual([0, 1]);
   });
 
-  test("Rover can execute a multifaceted command", () => {
-    let command = "RM";
+  test('Rover can execute a multifaceted command', () => {
+    let command = 'RM';
     answer = newRover.execute(command);
-    expect(answer).toEqual([[1, 0], "E"]);
+    expect(answer).toEqual([[1, 0], 'E']);
   });
 
   // Tests for rollover/ edge cases; right, left, top and bottom
 
-  test("Test for right roll over edge case", () => {
-    let command = "RMMMMMM"; // edge case off grid right
+  test('Test for right roll over edge case', () => {
+    let command = 'RMMMMMM'; // edge case off grid right
     answer = newRover.execute(command);
-    expect(answer).toEqual([[0, 0], "E"]);
+    expect(answer).toEqual([[0, 0], 'E']);
   });
 
-  test("Test for top roll over edge case", () => {
-    let command = "MMMMMM"; // edge case off grid top, rollover to initial start point
+  test('Test for top roll over edge case', () => {
+    let command = 'MMMMMM'; // edge case off grid top, rollover to initial start point
     answer = newRover.execute(command);
-    expect(answer).toEqual([[0, 0], "N"]);
+    expect(answer).toEqual([[0, 0], 'N']);
   });
 
-  test("Test for rotate roll over", () => {
-    let command = "RRRR"; // edge case off right of compassPoint, rollover to initial start point
+  test('Test for rotate roll over', () => {
+    let command = 'RRRR'; // edge case off right of compassPoint, rollover to initial start point
     answer = newRover.execute(command);
-    expect(answer).toEqual([[0, 0], "N"]);
+    expect(answer).toEqual([[0, 0], 'N']);
   });
 
-  test("Test for rotate roll over, on lefthand side", () => {
-    let command = "L"; // edge case off left of compassPoint, negative
+  test('Test for rotate roll over, on lefthand side', () => {
+    let command = 'L'; // edge case off left of compassPoint, negative
     answer = newRover.execute(command);
-    expect(answer).toEqual([[0, 0], "W"]);
+    expect(answer).toEqual([[0, 0], 'W']);
   });
 
-  test("Test for bottom roll over edge case", () => {
-    let command = "LLM"; // edge case off grid bottom, negative
+  test('Test for bottom roll over edge case', () => {
+    let command = 'LLM'; // edge case off grid bottom, negative
     answer = newRover.execute(command);
-    expect(answer).toEqual([[0, 5], "S"]);
+    expect(answer).toEqual([[0, 5], 'S']);
   });
 
   // Test cases as provided within Kata
 
-  test("Test case 1 from Mars Rover Kata; long scenerio", () => {
-    newRover = new marsRover([1, 2], "N", [5, 5]);
-    let command = "LMLMLMLMM";
+  test('Test case 1 from Mars Rover Kata; long scenerio', () => {
+    newRover = new marsRover([1, 2], 'N', [5, 5]);
+    let command = 'LMLMLMLMM';
     answer = newRover.execute(command);
-    expect(answer).toEqual([[1, 3], "N"]);
+    expect(answer).toEqual([[1, 3], 'N']);
   });
 
-  test("Test case 2 from Mars Rover Kata; long scenerio", () => {
-    newRover = new marsRover([3, 3], "E", [5, 5]);
-    let command = "MMRMMRMRRM";
+  test('Test case 2 from Mars Rover Kata; long scenerio', () => {
+    newRover = new marsRover([3, 3], 'E', [5, 5]);
+    let command = 'MMRMMRMRRM';
     answer = newRover.execute(command);
-    expect(answer).toEqual([[5, 1], "E"]);
+    expect(answer).toEqual([[5, 1], 'E']);
   });
 
   // Future Test for obstacle(s)
